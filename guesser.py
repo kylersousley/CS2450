19a5aed58fcf7745bee828efad4659aac5950ede
Kyler Sousley <kylersousley@gmail.com>
2025-01-14 18:12:04 -0700

    added acceptence for the answer yes and Yes

--- guesser.py
+++ guesser.py
@@ -10,9 +10,9 @@ while guessed == False:
     guess = random.randint(15,30)
     if guess in guesses:
         continue
-    
+
     user_response = input("Are you "+str(guess)+" years old? ")
-    if user_response == "y" or user_response == "Y":
+    if user_response == "y" or user_response == "Y" or user_response == "yes" or user_response == "Yes":
         print(f"Haha! {name} is " + str(guess) + " years old! I guessed it")
         guessed = True
 
